7783e9c12d734f4d993ecee974f367edaa845dc9
KingfernJohn <49335210+KingfernJohn@users.noreply.github.com>
2022-11-24 21:06:19 +0100

    Add files via upload

---
+++ src/main/java/io/github/spitfox/realisticnametag/Main.java
@@ -0,0 +1,23 @@
+package io.github.spitfox.realisticnametag;
+
+import com.mojang.logging.LogUtils;
+import io.github.spitfox.realisticnametag.events.E_PlayerInteract;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "realisticnametag";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Main()
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new E_PlayerInteract());
+
+    }
+
+}
---
+++ src/main/java/io/github/spitfox/realisticnametag/events/E_PlayerInteract.java
@@ -0,0 +1,14 @@
+package io.github.spitfox.realisticnametag.events;
+
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class E_PlayerInteract
+{
+    @SubscribeEvent
+    public void PlayerInteract(PlayerInteractEvent.RightClickBlock event)
+    {
+
+    }
+
+}
